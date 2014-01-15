d0cd509e0442c965ae82d24214ede5036c167de5
zumbojo <zumbojo@gmail.com>
2014-01-14 19:45:14 -0600

    Solution now builds with zero errors, zero warnings.

--- MyMvcApplication/Models/AccountModels.cs
+++ MyMvcApplication/Models/AccountModels.cs
@@ -8,6 +8,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 
+using Compare = System.ComponentModel.DataAnnotations.CompareAttribute;
+
 namespace MyMvcApplication.Models
 {
 
