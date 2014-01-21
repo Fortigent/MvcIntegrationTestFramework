efc518e60c1001c36f68e59a54449f86741bc12d
zumbojo <zumbojo@gmail.com>
2014-01-20 19:34:45 -0600

    Starting on getting Root_Url_Renders_Index_View() working.

--- MyMvcApplicaton.MSTests/UnitTest1.cs
+++ MyMvcApplicaton.MSTests/UnitTest1.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcIntegrationTestFramework.Browsing;
+using MvcIntegrationTestFramework.Hosting;
+using System.Web.Mvc;
 
 namespace MyMvcApplicaton.MSTests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private AppHost appHost;
+
+        //[TestFixtureSetUp]
+        public void TestFixtureSetUp()
         {
+            //If you MVC project is not in the root of your solution directory then include the path
+            //e.g. AppHost.Simulate("Website\MyMvcApplication")
+            appHost = AppHost.Simulate("MyMvcApplication");
+        }
+
+        [TestMethod]
+        public void Root_Url_Renders_Index_View()
+        {
+            TestFixtureSetUp();
+
+            appHost.Start(browsingSession =>
+            {
+                // Request the root URL
+                RequestResult result = browsingSession.Get("");
+
+                // Can make assertions about the ActionResult...
+                var viewResult = (ViewResult)result.ActionExecutedContext.Result;
+                Assert.AreEqual("Index", viewResult.ViewName);
+                Assert.AreEqual("Welcome to ASP.NET MVC!", viewResult.ViewData["Message"]);
+
+                // ... or can make assertions about the rendered HTML
+                Assert.IsTrue(result.ResponseText.Contains("<!DOCTYPE html"));
+            });
         }
     }
 }
