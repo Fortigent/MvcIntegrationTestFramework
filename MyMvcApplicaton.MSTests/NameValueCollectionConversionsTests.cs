260d2b9f8a1bdd2ef90d245d9bf9311933bfaa46
zumbojo <zumbojo@gmail.com>
2014-01-20 19:48:55 -0600

    Starting on NameValueCollectionConversionsTests.

---
+++ MyMvcApplicaton.MSTests/NameValueCollectionConversionsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyMvcApplicaton.MSTests
+{
+    [TestClass]
+    public class NameValueCollectionConversionsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
