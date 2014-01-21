39cf4ea5b436cfbe5ecef922343390d56d38bce4
zumbojo <zumbojo@gmail.com>
2014-01-20 19:53:10 -0600

    Should_have_key_of_name_with_value_hello() is passing.

--- MyMvcApplicaton.MSTests/NameValueCollectionConversionsTests.cs
+++ MyMvcApplicaton.MSTests/NameValueCollectionConversionsTests.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcIntegrationTestFramework;
+using System.Collections.Specialized;
 
 namespace MyMvcApplicaton.MSTests
 {
     [TestClass]
-    public class NameValueCollectionConversionsTests
+    public class When_converting_an_object_with_one_string_property_to_name_value_collection
     {
-        [TestMethod]
-        public void TestMethod1()
+        private NameValueCollection convertedFromObjectWithString;
+
+        public When_converting_an_object_with_one_string_property_to_name_value_collection()
         {
+            convertedFromObjectWithString = NameValueCollectionConversions.ConvertFromObject(new { name = "hello" });
+
+        }
+
+        [TestMethod]
+        public void Should_have_key_of_name_with_value_hello()
+        {
+            Assert.AreEqual("hello", convertedFromObjectWithString["name"]);
         }
     }
 }
